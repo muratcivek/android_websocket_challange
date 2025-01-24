e6ad53342cc3fb2d2d6292cf030bb5018455946f
Murat Civek <muratttcivek@gmail.com>
2025-01-24 22:01:10 +0300

    Proje oluşturuldu ve MVVm klasör yapısı entegre edildi.

---
+++ app/src/main/java/com/example/android_websocket_challange/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_websocket_challange.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_websocket_challange.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
